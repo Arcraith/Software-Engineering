66795617d16c7cdf75a4969d4050410d39f996aa
Arcraith <tobias.schweisfurth@web.de>
2015-05-04 19:01:27 +0200

    added Frage: Bist du wissbegierig?

--- Studienberatung.java
+++ Studienberatung.java
@@ -5,7 +5,13 @@ public class Studienberatung {
 		Scanner scanner = new Scanner(System.in);
 		String antwort = scanner.next();
 		if(antwort.equalsIgnoreCase("J")){
+			System.out.println("Bist du wissbegierig?");
+			antwort = scanner.next();
+			if(antwort.equalsIgnoreCase("J")){
 
+			} else if(antwort.equalsIgnoreCase("N")){
+				System.out.println("Nicht studieren");
+			}
 		} else if(antwort.equalsIgnoreCase("N")){
 			
 		}
